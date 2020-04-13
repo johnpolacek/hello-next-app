f6f9ffa75ec38a9e1a5adcb3520e044c0929a3bd
johnpolacek <johnpolacek@gmail.com>
2020-04-13 16:39:35 -0500

    update app config

--- app.config.marketing.js
+++ app.config.marketing.js
@@ -10,12 +10,11 @@ module.exports = {
   shortDescription: "Very Short App Description",
   // longDescription : appears in landing page meta description
   longDescription:
-    "Longer description of your app that will appear in search engine results and when people post links to your site on social media.",
-  // headline: appears as landing page headline
+    "React Web App Project Template for building application websites with the Next.js Framework, a Firebase Serverless Backend, Theme UI Components and more.",
+  // headline: appears as landing page headline (recommend keep it to )
   headline: "High Converting Headline Goes Here",
-  // subhead: appears below the landing page headline
   subhead:
-    "Subhead copy that explains the features and benefits of your app to your audience of potential users and captures their interest to get them to sign up or find out more",
+    "Launch your next web application with serverless backend, user authentication, automated testing, Stripe integration, a custom design theme and more.",
   // contact appears in the privacy policy page
-  contact: "contact@yourappdomain.com",
+  contact: "hellonextapp@gmail.com",
 }
