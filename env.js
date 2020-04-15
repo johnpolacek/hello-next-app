9e32ca1d9cb7a045009cc568114f504890f8e6fa
johnpolacek <johnpolacek@gmail.com>
2020-04-14 20:23:26 -0500

    update .env

--- env.js
+++ env.js
@@ -1,6 +1,5 @@
 // Responsible for setting environment variables.
-// Note: this isn't strictly required for this example – you can
-// inline your Firebase config or set environment variables howevever
+// you can inline your Firebase config or set environment variables howevever
 // else you wish – but it's a convenient way to make sure the private
 // key doesn't end up in source control.
 
