f8b0bb64bb27650bff8e33de95d1b18530eafdc4
johnpolacek <johnpolacek@gmail.com>
2020-04-27 16:35:38 -0500

    move cancel plan link to manage plan page

--- components/views/Plan.js
+++ components/views/Plan.js
@@ -17,7 +17,7 @@ export default (props) => {
 
   return (
     <Box sx={{ textAlign: "center", width: "100%", color: "white", pb: 5 }}>
-      <Heading variant="headline">Your Account</Heading>
+      <Heading variant="headline">Manage Plan</Heading>
       <Text sx={{ pb: 4, fontStyle: "italic" }}>{error}&nbsp;</Text>
       {props.plan ? (
         <>
@@ -67,10 +67,15 @@ export default (props) => {
               exp. {props.plan.expires}
             </Text>
           </DataRow>
+          <Box sx={{ pt: 4, fontSize: 1 }}>
+            <NextLink href="../cancel" passHref>
+              <Link variant="reverse">Cancel Account</Link>
+            </NextLink>
+          </Box>
         </>
       ) : (
         <Box sx={{ pb: 6, mb: 6 }}>
-          <Heading variant="subhead">Your account is inactive.</Heading>
+          <Heading variant="subhead">Your account has been cancelled.</Heading>
           <ButtonLink bg="secondary" fontSize={4} href="/plans">
             Activate New Plan
           </ButtonLink>
---
+++ pages/account/plan.js
@@ -0,0 +1,46 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { get } from "lodash/object"
+import withAuthUser from "../../utils/context/withAuthUser"
+import withAuthUserInfo from "../../utils/context/withAuthUserInfo"
+import appConfig from "../../app.config"
+import Wrapper from "../../components/layout/Wrapper"
+import Plan from "../../components/views/Plan"
+import getPlan from "../../utils/firebase/getPlan"
+
+const Page = (props) => {
+  const AuthUser = get(props.AuthUserInfo, "AuthUser", null)
+
+  return (
+    <Wrapper
+      url="/"
+      title={appConfig.name + " | Manage Plan"}
+      description={"Your " + appConfig.name + " account information"}
+      bg="primary"
+    >
+      {AuthUser && <Plan {...props} />}
+    </Wrapper>
+  )
+}
+
+Page.getInitialProps = async (ctx) => {
+  // Get the AuthUserInfo object. This is set in `withAuthUser.js`.
+  // The AuthUserInfo object is available on both the server and client.
+  const AuthUserInfo = get(ctx, "myCustomData.AuthUserInfo", null)
+  const AuthUser = get(AuthUserInfo, "AuthUser", null)
+
+  // You can fetch data here.
+  return await getPlan(AuthUser.id).then((plan) => {
+    return { plan: plan }
+  })
+}
+
+Page.defaultProps = {
+  AuthUserInfo: null,
+}
+
+// Use `withAuthUser` to get the authed user server-side, which
+// disables static rendering.
+// Use `withAuthUserInfo` to include the authed user as a prop
+// to your component.
+export default withAuthUser(withAuthUserInfo(Page))
