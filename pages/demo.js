b1bd6e53002cb1c50bb2e9ae73014bd74d768d33
johnpolacek <johnpolacek@gmail.com>
2020-05-05 13:01:52 -0500

    code formatting

--- pages/demo.js
+++ pages/demo.js
@@ -25,7 +25,7 @@ export const getServerSideProps = withSession(async function ({ req, res }) {
     return
   } else {
     const user = req.session.get("user")
-    return { props: { user }}
+    return { props: { user } }
   }
 })
 
