8f347aae4c271c0e26400d963a3e4a6eb8a715fc
johnpolacek <johnpolacek@gmail.com>
2020-05-13 05:59:24 -0500

    fix view account for free plan

--- cypress/integration/SignUp.js
+++ cypress/integration/SignUp.js
@@ -19,18 +19,22 @@ describe("Sign Up", function () {
   // })
 
   it("for a free starter account", function () {
+    cy.exec(
+      "export GOOGLE_APPLICATION_CREDENTIALS='./lib/firebase/admin/firebase-adminsdk-ykx60-eb3ef0c1c5.json' && node ./lib/firebase/admin/deleteTestUser.js"
+    )
+
     cy.visit("/")
     cy.get("a").contains("Sign Up").click()
     cy.get("h2").contains("Create an account").should("be.visible")
     cy.get("input[name=email]").type("test@hellonextapp.com")
-    cy.get("input[name=password]").type("aa" + Math.random())
+    cy.get("input[name=password]").type("TestSignUp123!")
     cy.get("input[type=checkbox]").parent().click()
     cy.get("form").find("button").contains("Sign Up").click()
     cy.wait(2000)
     cy.get("h2")
       .contains("Choose the plan that’s right for you")
       .should("be.visible")
-    cy.get("a[href='./plans/starter']").click()
+    cy.get("a[href='./plans/starter/ready']").click()
     cy.wait(2000)
     cy.get("h2").contains("All Set!").should("be.visible")
 
