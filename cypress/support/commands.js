9d81d3affd1c5b01228689503ca0828f0166c43f
johnpolacek <johnpolacek@gmail.com>
2020-05-16 15:47:13 -0500

    add wait to fill out cc num

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,7 @@ Cypress.Commands.add("getWithinIframe", (targetElement) =>
 
 Cypress.Commands.add("fillOutCreditCardForm", () => {
   const exp = "12" + (new Date().getFullYear() + 1).toString().substring(2)
+  cy.wait(500)
   cy.getWithinIframe('[name="cardnumber"]').type("4242424242424242")
   cy.wait(2000)
   cy.getWithinIframe('[name="exp-date"]').type(exp)
