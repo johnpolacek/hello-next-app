860adbe17ea9eeafcaf7943cd5b186a6ae5d380e
johnpolacek <johnpolacek@gmail.com>
2020-07-03 13:57:14 -0500

    update free user account email

--- pages/api/updateEmail.js
+++ pages/api/updateEmail.js
@@ -9,19 +9,24 @@ const updateEmail = withSession(async (req, res) => {
   let user = req.session.get("user")
 
   try {
-    stripe.customers.update(
-      stripeId,
-      {email},
-      async (err, customer) => {
-        console.log("stripe.customers.update err", err)
-        console.log("stripe.customers.update customer", customer)
-        const data = await updateAuthUserProperty(token, "email", email)
-        user.email = email
-        req.session.set("user", user)
-        await req.session.save()
-        return res.status(200).json({ data })
-      }
-    )
+    const data = await updateAuthUserProperty(token, "email", email)
+    user.email = email
+    req.session.set("user", user)
+    await req.session.save()
+    if (stripeId === -1) {
+      return res.status(200).json({ data })
+    } else {
+      stripe.customers.update(
+        stripeId,
+        {email},
+        async (err, customer) => {
+          console.log("stripe.customers.update err", err)
+          console.log("stripe.customers.update customer", customer)
+          return res.status(200).json({ data })
+        }
+      )
+    }
+    
   } catch (error) {
     console.log("error", error)
     return res.status(401).json({ result: "error" }, error)
