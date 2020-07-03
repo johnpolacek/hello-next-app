ca705444d4174d7e2ffd2d84be8bd1e10e0f71e0
johnpolacek <johnpolacek@gmail.com>
2020-07-03 12:58:04 -0500

    wip update account email

--- pages/api/updateEmail.js
+++ pages/api/updateEmail.js
@@ -1,16 +1,27 @@
 import withSession from "../../lib/session"
 import updateAuthUserProperty from "../../lib/firebase/admin/updateAuthUserProperty"
+import Stripe from "stripe"
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_TEST)
 
 const updateEmail = withSession(async (req, res) => {
-  const { token, email } = req.body
+
+  const { token, email, stripeId } = req.body
   let user = req.session.get("user")
 
   try {
-    const data = await updateAuthUserProperty(token, "email", email)
-    user.email = email
-    req.session.set("user", user)
-    await req.session.save()
-    return res.status(200).json({ data })
+    stripe.customers.update(
+      stripeId,
+      {email},
+      async (err, customer) => {
+        console.log("stripe.customers.update err", err)
+        console.log("stripe.customers.update customer", customer)
+        const data = await updateAuthUserProperty(token, "email", email)
+        user.email = email
+        req.session.set("user", user)
+        await req.session.save()
+        return res.status(200).json({ data })
+      }
+    )
   } catch (error) {
     console.log("error", error)
     return res.status(401).json({ result: "error" }, error)
