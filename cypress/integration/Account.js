2fea90ba5cec6ba1e4544fea121da4c732baade2
johnpolacek <johnpolacek@gmail.com>
2020-07-03 13:16:37 -0500

    add test for update paid user email

--- cypress/integration/Account.js
+++ cypress/integration/Account.js
@@ -1,7 +1,7 @@
-describe("Account Page", function () {
-  it("displays account info", function () {
+describe("Paid Account", function () {
+  it("can view account info", function () {
     cy.fixture("users").then((users) => {
-      cy.login()
+      cy.loginPaid()
       cy.visit("/account")
       cy.get("label").contains("Email").should("be.visible")
       cy.get("label").contains("Password").should("be.visible")
@@ -16,11 +16,37 @@ describe("Account Page", function () {
 
   it("can change email", function () {
     cy.fixture("users").then((users) => {
-      cy.login()
+
+      const newEmail = "temp@hellonextapp.com"
+
+      // change email
+      cy.loginPaid()
       cy.visit("/account")
-      cy.get("#accountInfo button").eq(0).click()
+      cy.get("#accountInfo button").contains("update").eq(0).click()
       cy.get("input[type=email]").clear()
-      cy.get("input[type=email]").type("temp@hellonextapp.com")
+      cy.get("input[type=email]").type(newEmail)
+      cy.get("button").contains("save").click()
+      cy.wait(4000)
+      cy.get("div").contains(newEmail).should("be.visible")
+
+      // logout then sign in with new email
+      cy.get("button").contains("Logout").click()
+      cy.wait(1000)
+      cy.get("input[name=email]").type(newEmail)
+      cy.get("input[name=password]").type(users.paid.password)
+      cy.get("form").find("button").contains("Login").click()
+      cy.wait(2000)
+      cy.get("p").contains("Signed In View of App").should("be.visible")
+      
+      // change back
+      cy.visit("/account")
+      cy.get("div").contains(newEmail).should("be.visible")
+      cy.get("#accountInfo button").contains("update").eq(0).click()
+      cy.get("input[type=email]").clear()
+      cy.get("input[type=email]").type(users.paid.email)
+      cy.get("button").contains("save").click()
+      cy.wait(4000)
+      cy.get("div").contains(users.paid.email).should("be.visible")
     })
   })
 })
