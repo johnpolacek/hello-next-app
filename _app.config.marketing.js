39c38439eb5cfa90684bd235effd036538ce84e7
johnpolacek <johnpolacek@gmail.com>
2020-07-18 15:43:15 -0500

    update gitignore

--- _app.config.marketing.js
+++ _app.config.marketing.js
@@ -5,12 +5,12 @@ module.exports = {
     local: "http://localhost:3000",
   },
   // name : appears in page titles, header nav
-  name: "Your App Name",
+  name: "Hello Next App",
   // shortDescription : appears in landing page title
-  shortDescription: "Very Short App Description",
+  shortDescription: "Next.js Web App Template",
   // longDescription : appears in landing page meta description
   longDescription:
-    "React Web App Project Template for building application websites with the Next.js Framework, a Firebase Serverless Backend, Theme UI Components and more.",
+    "Web App Project Template for building application websites with the Next.js Framework, a Firebase Serverless Backend, Theme UI Components and more.",
   // headline: appears as landing page headline (recommend keep it to )
   headline: "High Converting Headline Goes Here",
   subhead:
