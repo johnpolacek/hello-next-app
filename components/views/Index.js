b715d43c6c16e479fbd243d70ceb790a47e954d7
johnpolacek <johnpolacek@gmail.com>
2020-07-19 15:41:58 -0500

    add gumroad download option for landing page

--- components/views/Index.js
+++ components/views/Index.js
@@ -1,4 +1,5 @@
-import { Box, Heading, Text } from "theme-ui"
+/** @jsx jsx */
+import { jsx, Box, Heading, Text } from "theme-ui"
 import Link from "next/link"
 import ButtonLink from "../ui/nav/ButtonLink"
 import appConfig from "../../app.config"
@@ -17,10 +18,33 @@ const Index = (props) => (
         </Heading>
         <Box>
           <Box sx={{ display: "inline-block", mx: 3 }}>
-            <ButtonLink fontSize={4} bg="red" href="/signup">
+            <ButtonLink fontSize={4} bg="secondary" href="/signup">
               Create Account
             </ButtonLink>
           </Box>
+          {appConfig.gumroad && (
+            <Box sx={{ display: "inline-block", mx: 3, position: "relative" }}>
+              <script src="https://gumroad.com/js/gumroad.js"></script>
+              <a
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  opacity: 0,
+                  bg: "yellow",
+                  width: "100%",
+                  height: "100%",
+                }}
+                class="gumroad-button"
+                href="https://gum.co/vUpFK"
+              >
+                Download
+              </a>
+              <ButtonLink fontSize={4} bg="secondary" href="#" download>
+                Download
+              </ButtonLink>
+            </Box>
+          )}
           <Box sx={{ display: "inline-block", mx: 3 }}>
             <ButtonLink fontSize={4} bg="white" href="/demo">
               View Demo
