9a9c7cf039808e8cb33d8fdddc427002c6be4308
johnpolacek <johnpolacek@gmail.com>
2020-12-26 21:22:26 -0600

    update privacy and terms content, update deps

--- cypress/integration/LandingPage.js
+++ cypress/integration/LandingPage.js
@@ -4,6 +4,18 @@ describe("Landing Page", function () {
     cy.get("h1").should("be.visible")
   })
 
+  describe("Footer", function () {
+    it("has link to privacy page", function () {
+      cy.get("footer a").contains("Privacy Policy").click()
+      cy.get("#privacy", { timeout: 10000 }).contains("Privacy policy").should("be.visible")
+    })
+
+    it("can has link to privacy page", function () {
+      cy.get("footer a").contains("Terms of Service").click()
+      cy.get("#terms", { timeout: 10000 }).contains("Terms of Service").should("be.visible")
+    })
+  })
+
   describe("Create Account Button", function () {
     it("links to Sign Up Form", function () {
       cy.visit("/")
