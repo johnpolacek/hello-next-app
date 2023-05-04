ba116dddca4e66b4b5db5ea5ae2ab6d990ba2d3c
John Polacek <johnpolacek@gmail.com>
2023-05-04 08:32:40 -0500

    update footer styling

--- src/components/ui/layout/Footer.tsx
+++ src/components/ui/layout/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="sm:grid grid-cols-2 sm:px-8 md:px-16 py-16 bg-white w-full border-b">
-      <div className="flex flex-col sm:flex-row gap-8">
+    <footer className="md:grid grid-cols-2 md:px-8 md:px-16 py-16 bg-white w-full border-b">
+      <div className="flex flex-col items-center md:flex-row gap-8">
         <p>
           Created by{" "}
           <a className="text-indigo-600" href="https://johnpolacek.com">
@@ -17,7 +17,7 @@ export default function Footer() {
           </a>
         </p>
       </div>
-      <div className="flex flex-col sm:flex-row gap-8 justify-end">
+      <div className="flex flex-col md:flex-row mt-8 md:mt-0 gap-8 items-center justify-end">
         <Link href="/privacy" className="text-indigo-600">
           Privacy Policy
         </Link>
